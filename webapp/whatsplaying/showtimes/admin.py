d422d1d141b2e7b8f40d023af7741abcd70d89c9
Nick Salvatore <nmsalvatore@gmail.com>
2025-04-26 18:46:10 -0700

    feat: register Movie, Showtime, Venue models in admin

--- webapp/whatsplaying/showtimes/admin.py
+++ webapp/whatsplaying/showtimes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie, Showtime, Venue
+
+admin.site.register(Movie)
+admin.site.register(Showtime)
+admin.site.register(Venue)
